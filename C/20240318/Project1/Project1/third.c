8a62e2ae814e232cdb737e68abd075aed6d8be28
Mourn5367 <qwerrrt6@gmail.com>
2024-03-18 22:00:25 +0900

    알것같은데 생각좀 해봐

---
+++ C/20240318/Project1/Project1/third.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+	int c = 0;
+	int r = 0;
+
+	int i = 0;
+	int j = 0;
+
+
+	scanf_s("%d", &c);
+
+	scanf_s("%d", &r);
+
+
+
+	//for ( i = 0, j = 0; i < c; i++,j++ )
+	//{
+	//	
+	//	//if (i >= j)
+	//	//{
+	//	//	printf("* ");
+
+	//	//}
+
+	//	printf("* ");
+
+	//	if (c - 1 == i)
+	//	printf("\n");
+
+	//}
+
+	for (i = 0; i < c; i++)
+	{
+		
+
+			printf("* ");
+		
+	}
+
+	printf("SAD");
+	
+
+	return 0;
+}
