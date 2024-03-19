ab85a046053314b143970c8bc01476661ff13a0c
Mourn5367 <qwerrrt6@gmail.com>
2024-03-19 19:21:54 +0900

    도움말 입력 및 수정

--- C/20240318/Project1/Project1/third.c
+++ C/20240318/Project1/Project1/third.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
-
-
-void tri_1(int row)
+int tri_1_2(int row)
 {
 	for (int j = 0; j <= row; j++)
 	{
+		printf(" *");
 		if (j == row)
 		{
 			printf("\n");
 			break;
 		}
-		printf(" *");
+	}
+}
+int tri_1_1(int input_Num_value)
+{
+	int i = 0;
+
+	for (i = 0; i < input_Num_value; i++)
+	{
+		tri_1_2(i);
 	}
 }
 
 int main(void)
 
 {
-	int c = 0;
-	int i = 0;
+	int input_Num = 0;
 
-	scanf_s("%d", &c);	
+	printf("표현하고자 하는 길이를 입력하시면 *로 꽉찬 직각삼각형이 출력 됩니다.\n");
 
-	for (i = 0; i < c; i++)
-	{
-		tri_1(i);
-		//void asdf(int row)
-		//{
-		//	for (int j = 0; j <= row; j++)
-		//	{
-		//		if (j == row)
-		//		{
-		//			printf("\n");
-		//			break;
-		//		}
-		//		printf(" *");
-		//	}
-		//}
-	}
+	scanf_s("%d", &input_Num);
+
+	tri_1_1(input_Num);
 
 	return 0;
 }
+
+
