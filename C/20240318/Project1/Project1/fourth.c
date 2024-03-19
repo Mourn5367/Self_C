e89ecf51e3e939494a7af82f3297aa4438366587
Mourn5367 <qwerrrt6@gmail.com>
2024-03-19 19:03:25 +0900

    도움말 입력

--- C/20240318/Project1/Project1/fourth.c
+++ C/20240318/Project1/Project1/fourth.c
@@ -28,7 +28,7 @@ int main(void)
 {
 	int input_Num = 0;
 
-	printf("표현하고자 하는 길이를 입력하시면 꽉찬 정사각형이 출력 됩니다.\n");
+	printf("표현하고자 하는 길이를 입력하시면 *로 꽉찬 정사각형이 출력 됩니다.\n");
 
 	scanf_s("%d", &input_Num);
 	square_1(input_Num);
