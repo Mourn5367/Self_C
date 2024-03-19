c3f0f28264d0442e2e1b8ff2ca19e7b30f060d48
Mourn5367 <qwerrrt6@gmail.com>
2024-03-19 19:46:40 +0900

    주석을 더 달아놓는게 아니라면

--- C/20240318/Project1/Project1/third.c
+++ C/20240318/Project1/Project1/third.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-int tri_1_2(int row)
+
+void tri_1_2(int row)
 {
+	
 	for (int j = 0; j <= row; j++)
 	{
-		printf(" *");
+		
 		if (j == row)
 		{
-
-			printf("\n");
+			printf("*\n");
 			break;
-
 		}
+		else printf("* ");
 	}
 }
-int tri_1_1(int input_Num_value)
+
+void tri_1_1(int input_Num_value)
 {
 	int i = 0;
 
 	for (i = 0; i < input_Num_value; i++)
 	{
-
 		tri_1_2(i);
-
 	}
 }
 
@@ -32,7 +32,8 @@ int main(void)
 
 	printf("표현하고자 하는 길이를 입력하시면 *로 꽉찬 직각삼각형이 출력 됩니다.\n");
 
-	scanf_s("%d", &input_Num);
+	scanf_s("%d", &input_Num);printf("\n");
+
 	tri_1_1(input_Num);
 
 	return 0;
